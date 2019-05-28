f6281fc9cd7202f678ce7ea01752c0522364996e
Lord Hex <peachesappprivate@gmail.com>
2019-05-28 16:28:54 +0800

    Transforms

--- src/views/Claim.vue
+++ src/views/Claim.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="claim">
+  <div class="container">
   </div>
 </template>
 
--- src/views/Transform.vue
+++ src/views/Transform.vue
@@ -1,5 +1,88 @@
 <template>
-  <div class="transform">
+  <div class="container">
+    <h4 class="title is-4 has-text-centered">Transform Days</h4>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">Day 4 / 351</p>
+            <p class="subtitle is-5">332 Hex Available</p>
+          </div>
+          <div class="media-right">
+            <b>Your HEX in Lobby:</b> 9.0183867745 HEX<br>
+          </div>
+        </div>
+        <div class="content">
+          <b>All ETH in Lobby:</b> 4.1231321 ETH<br>
+          <b>Your ETH in Lobby:</b> 0.112 ETH<br>
+          <b>Ends In:</b> 21:51:10<br><br>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a class="card-footer-item">Join Lobby</a>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">Day 3 / 351</p>
+            <p class="subtitle is-5">341 Hex Available</p>
+          </div>
+          <div class="media-right">
+            <b>Your HEX in Lobby:</b> 0 HEX<br>
+          </div>
+        </div>
+        <div class="content">
+          <b>All ETH in Lobby:</b> 9.98 ETH<br>
+          <b>Your ETH in Lobby:</b> 0 ETH<br>
+          <b>ENDED</b>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">Day 2 / 351</p>
+            <p class="subtitle is-5">996 Hex Available</p>
+          </div>
+          <div class="media-right">
+            <b>Your HEX in Lobby:</b> 0 HEX<br>
+          </div>
+        </div>
+        <div class="content">
+          <b>All ETH in Lobby:</b> 8.14 ETH<br>
+          <b>Your ETH in Lobby:</b> 1.45 ETH<br>
+          <b>ENDED</b><br><br>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a class="card-footer-item">Transfer to Wallet</a>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">Day 1 / 351</p>
+            <p class="subtitle is-5">1000 Hex Available</p>
+          </div>
+          <div class="media-right">
+            <b>Your HEX in Lobby:</b> 177.4201474201 HEX<br>
+          </div>
+        </div>
+        <div class="content">
+          <b>All ETH in Lobby:</b> 3.16 ETH<br>
+          <b>Your ETH in Lobby:</b> 0 ETH<br>
+          <b>ENDED</b>
+        </div>
+      </div>
+    </div>
+    <br>
   </div>
 </template>
 
