f8bd5989317e88f75c09fdf4a48376974035c8cb
Lord Hex <peachesappprivate@gmail.com>
2019-05-31 07:02:09 +0800

    Referral Address

--- src/views/Refer.vue
+++ src/views/Refer.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="container">
-
+    <div class="card has-text-centered">
+      <div class="card-content">
+        <div class="content">
+          <h4 class="title is-4 has-text-centered">Your Referral Address</h4>
+          <div class="field has-addons">
+            <div class="control is-expanded">
+              <input class="input" type="text" :value="refLink">
+            </div>
+            <div class="control">
+              <button class="button is-info" v-clipboard:copy="this.refLink">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'refer',
-  components: {
+  data() {
+    return {
+      refLink: 'https://hex.win/?r=0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE',
+    };
   },
+  components: {},
 };
 </script>
