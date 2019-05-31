c7cdeb3d757cea88d06dc42f10c0e6b796657ad2
Lord Hex <peachesappprivate@gmail.com>
2019-05-31 09:54:38 +0800

    Nav

--- src/views/Claim.vue
+++ src/views/Claim.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="container">
+  <div>
+    <div class="tabs is-centered">
+      <ul>
+        <li class="is-active"><a>Find Claim</a></li>
+        <i class="fas fa-chevron-right"></i>
+        <li><a>Review Claim</a></li>
+        <i class="fas fa-chevron-right"></i>
+        <li><a>Sign Claim Message</a></li>
+        <i class="fas fa-chevron-right"></i>
+        <li><a>Submit</a></li>
+      </ul>
+    </div>
 
+    <div class="container">
+
+    </div>
   </div>
 </template>
 
