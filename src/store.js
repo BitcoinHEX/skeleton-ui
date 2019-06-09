e74c1eebe478844060367192b0d0f0f4fe82bbea
Lord Hex <peachesappprivate@gmail.com>
2019-06-10 00:43:48 +0800

    Initial

--- src/store.js
+++ src/store.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import hexWalletUtils from 'hex-wallet-utils';
 
 Vue.use(Vuex);
+
+function updateState() {
+  // Do Updates
+}
+
+hexWalletUtils.subscribe('block', updateState);
 
 export default new Vuex.Store({
   state: {
